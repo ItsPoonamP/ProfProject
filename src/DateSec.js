1289d567385055b3503af7a09ba987516886d4ff
ItsPoonamP <147233308+ItsPoonamP@users.noreply.github.com>
2024-07-18 15:47:22 +0530

    Add files via upload

---
+++ src/DateSec.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function DateSec() {
+  return (
+    <div className='date'>
+      Key Dates
+      <div className="info">
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      </div>
+    </div>
+  )
+}
