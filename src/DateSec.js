750c3f2ca71b47bfe5e339a6748323f322933790
ItsPoonamP <ponamkpatil31@gmail.com>
2024-07-22 23:05:54 +0530

    Date-update

--- src/DateSec.js
+++ src/DateSec.js
@@ -5,10 +5,14 @@ export default function DateSec() {
     <div className='date'>
      <p> Key Dates</p>
       <div className="info">
-      <div className="box">20-9-2024</div>
-      <div className="box">30-9-2024</div>
-      <div className="box">20-10-2024</div>
-      <div className="box">13-12-2024</div>
+      <div className="box">20-9-2024
+      <div className="tx">Notification of Acceptance of Manuscript</div></div>
+      <div className="box"> 30-9-2024  
+      <div className="tx">Last Date for Submission of Camera Ready Paper</div></div>
+      <div className="box"> 20-10-2024  
+      <div className="tx">Early Bird Registration</div></div>
+      <div className="box"> 13-12-2024  
+      <div className="tx">Last Date for Registration</div></div>
       </div>
     </div>
   )
